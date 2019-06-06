90bd5e6e1f6a233ee3a6b64d8385ab4c83e99242
pvrreddy88 <51439671+pvrreddy88@users.noreply.github.com>
2019-06-06 20:50:58 +0530

    Update settings.py

--- instituteproject/instituteproject/settings.py
+++ instituteproject/instituteproject/settings.py
@@ -77,28 +77,28 @@ WSGI_APPLICATION = 'instituteproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'institutedb',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mydb',
-#         'USER':'postgres',
-#         'PASSWORD':'raja',
-#         "HOST":'localhost',
-#         'PORT':'5432'
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'institutedb',
+#         'USER':'root',
+#         'PASSWORD':'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER':'postgres',
+        'PASSWORD':'raja',
+        "HOST":'localhost',
+        'PORT':'5432'
+    }
+}
 
 
 # Password validation
